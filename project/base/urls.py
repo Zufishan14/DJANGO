74c201ef83024cfb45c934d7436538eba5eae563
Zufishan14 <mohammedzufishan@gmail.com>
2025-02-20 11:26:33 +0530

    Second Commit

--- project/base/urls.py
+++ project/base/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .import views
 
-urlpaterns = [
+urlpatterns = [
     path('', views.home, name='home'),
-    path('room', views.room, name='room'),
+    path('room/', views.room, name='room'),
 ]
 
--- project/base/views.py
+++ project/base/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'home.html')
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'room.html')
